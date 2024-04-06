08632551b0a97a0a51cdeb4ffb9e874e47448c84
Wakil Ahammad <69745195+Wakil-Ahammad@users.noreply.github.com>
2024-04-06 11:21:58 +0600

    Create Distinct Numbers.cpp

---
+++ Sorting and Searching/Distinct Numbers.cpp
@@ -0,0 +1,51 @@
+// https://cses.fi/problemset/task/1621
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define f first
+#define s second
+#define in insert
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define er erase
+#define pi 3.141592653589793238
+#define MOD 1000000007
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define INF (ll)1000000000000000
+#define all(x) x.begin(),x.end()
+//#define end cout<<endl
+#define bcntz __builtin_ctz
+#define fl(i,n) for(int i=0;i<n;i++)
+using namespace std;
+#define st(s) sort(s.begin(),s.end())
+ 
+ 
+void solve()
+{
+   int n;
+   cin>>n;
+   set<int>st;
+   for(int i=0;i<n;i++){
+      int x;
+      cin>>x;
+      st.insert(x);
+   }
+ 
+   cout<<st.size()<<endl;
+}
+ 
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+ 
+  int t=1;
+  //cin>>t;
+  while(t--)
+  {
+     solve();
+  }
+    
+}

ce0220df003e8a0fb89e36143c4244eeafa68ece
Wakil Ahammad <69745195+Wakil-Ahammad@users.noreply.github.com>
2024-04-06 11:20:23 +0600

    Create Apartments.cpp

---
+++ Sorting and Searching/Apartments.cpp
@@ -0,0 +1,66 @@
+// https://cses.fi/problemset/task/1084
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define f first
+#define s second
+#define in insert
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define er erase
+#define pi 3.141592653589793238
+#define MOD 1000000007
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define INF (ll)1000000000000000
+#define all(x) x.begin(),x.end()
+//#define end cout<<endl
+#define bcntz __builtin_ctz
+#define fl(i,n) for(int i=0;i<n;i++)
+using namespace std;
+#define st(s) sort(s.begin(),s.end())
+ 
+ 
+void solve()
+{
+   int n,m,k;
+   cin>>n>>m>>k;
+   int people[n], appart[m];
+   for(int i=0;i<n;i++) cin>>people[i];
+   for(int i=0;i<m;i++) cin>>appart[i];
+ 
+   sort(people,people+n);
+   sort(appart,appart+m);
+ 
+   int i=0,j=0,cnt = 0;
+   
+   while(i<n && j<m){
+      if(abs(people[i] - appart[j])<=k){
+         i++;
+         j++;
+         cnt++;
+      }
+ 
+      else if(people[i] < appart[j]) i++;
+ 
+      else j++;
+   }
+ 
+   cout<<cnt<<endl;
+  
+}
+ 
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+ 
+  int t=1;
+  //cin>>t;
+  while(t--)
+  {
+     solve();
+  }
+    
+}
